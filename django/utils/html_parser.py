a06fecde516210a7f8fc3f1ca1b11df9e17ba898
Timothy Andry <timothy.andry@atf.gov>
2025-09-16 15:27:32 -0500

    EREGS-255: Removed django.utils.six import.

--- django/utils/html_parser.py
+++ django/utils/html_parser.py
@@ -1,5 +1,6 @@
 from django.utils import six
 # from django.utils.six.moves import html_parser as _html_parser
+from html import parser as _html_parser
 
 try:
     HTMLParseError = _html_parser.HTMLParseError
